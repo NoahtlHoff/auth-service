922461276b516f5691a343457054788d0de42616
NoahtlHoff <noah.hoff@outlook.com>
2026-05-02 09:40:39 +0200

    add retryonfailure for azure connection errors

--- AuthService.Api/Program.cs
+++ AuthService.Api/Program.cs
@@ -13,7 +13,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sql => sql.EnableRetryOnFailure()));
 
 builder.Services.AddAuthorization();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
